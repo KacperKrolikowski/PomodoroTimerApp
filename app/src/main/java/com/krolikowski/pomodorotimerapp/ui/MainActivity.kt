742485cdaace873958c07da358e68c1b05070105
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-06-14 14:55:57 +0200

    Database from plan and start of Hilt

--- app/src/main/java/com/krolikowski/pomodorotimerapp/ui/MainActivity.kt
+++ app/src/main/java/com/krolikowski/pomodorotimerapp/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.krolikowski.pomodorotimerapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.krolikowski.pomodorotimerapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/krolikowski/pomodorotimerapp/ui/fragments/PreferencesFragment.kt
@@ -0,0 +1,9 @@
+package com.krolikowski.pomodorotimerapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.krolikowski.pomodorotimerapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PreferencesFragment: Fragment(R.layout.fragment_preferences) {
+}
