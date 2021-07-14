80634e82e1f3f5931a399cbf627f29c306567f89
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-07-14 20:30:38 +0200

    unlabeled

--- app/src/main/java/com/krolikowski/pomodorotimerapp/ui/MainActivity.kt
+++ app/src/main/java/com/krolikowski/pomodorotimerapp/ui/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.krolikowski.pomodorotimerapp.ui
 
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.krolikowski.pomodorotimerapp.R
+import com.krolikowski.pomodorotimerapp.ui.fragments.QuickPomodoroFragment
+import com.krolikowski.pomodorotimerapp.ui.viewmodels.QuickPomodoroViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var preferenceViewModel: QuickPomodoroViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        preferenceViewModel = ViewModelProvider(this).get(QuickPomodoroViewModel::class.java)
+
+
+
+        setLocate("pl")
 
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         navHostFragment.findNavController()
@@ -25,4 +41,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
+    private fun setLocate(Lang: String) {
+
+        val locale = Locale(Lang)
+
+        Locale.setDefault(locale)
+
+        val config = Configuration()
+
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
+    }
+
 }
