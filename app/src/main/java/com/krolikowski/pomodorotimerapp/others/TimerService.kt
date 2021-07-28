6741ecea0a1e19fee0fe4f7b6af858bd753de8a5
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-07-28 19:38:46 +0200

    Working service

--- app/src/main/java/com/krolikowski/pomodorotimerapp/others/TimerService.kt
+++ app/src/main/java/com/krolikowski/pomodorotimerapp/others/TimerService.kt
@@ -1,18 +1,72 @@
 package com.krolikowski.pomodorotimerapp.others
 
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.krolikowski.pomodorotimerapp.ui.MainActivity
+import com.krolikowski.pomodorotimerapp.ui.fragments.QuickPomodoroFragment
 
 class TimerService: Service() {
+
+    private lateinit var timer: CountDownTimer
+
+    private var timerState: String = "Running"
+    private var timerSinglePomodoroLength = 25L
+    private var timerPomodoroQuantity = 4
+    private var timerShortBreakTime = 5L
+    private var timerSecondsRemaining = 0L
+    private var timeToCount = 0L
+
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        Log.d("DEBUG_SERVICE", "here")
 
+        /*
+        val notificationIntent = Intent(this, QuickPomodoroFragment::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        val notification = NotificationCompat.Builder(this, "TimerService")
+            .setContentTitle("Pomodoro timer")
+            .setContentText("00:00")
+            .setContentIntent(pendingIntent)
+            .build()
 
-        return super.onStartCommand(intent, flags, startId)
+         */
+
+        timeToCount = intent!!.getLongExtra("TIME_TO_COUNT", 25L)
+
+        timer = object : CountDownTimer(timeToCount * 1000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                timeToCount = millisUntilFinished / 1000
+                Log.d("DEBUG_SERVICE", timeToCount.toString())
+                val backIntent = Intent(TIMER_UPDATE)
+                backIntent.putExtra(TIME_EXTRA, timeToCount)
+                sendBroadcast(backIntent)
+            }
+
+            override fun onFinish() {
+
+            }
+        }.start()
+        //startForeground(1, notification)
+
+        return START_NOT_STICKY
     }
 
+    override fun onDestroy() {
+
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    companion object{
+        const val TIMER_UPDATE = "timerUpdate"
+        const val TIME_EXTRA = "timeExtra"
+    }
 
 }
--- app/src/main/java/com/krolikowski/pomodorotimerapp/ui/fragments/QuickPomodoroFragment.kt
+++ app/src/main/java/com/krolikowski/pomodorotimerapp/ui/fragments/QuickPomodoroFragment.kt
@@ -1,25 +1,20 @@
 package com.krolikowski.pomodorotimerapp.ui.fragments
 
-import android.app.AlarmManager
-import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.snackbar.Snackbar
 import com.krolikowski.pomodorotimerapp.R
-import com.krolikowski.pomodorotimerapp.data.repositories.PomodoroTimerRepository
-import com.krolikowski.pomodorotimerapp.others.TimerExpiredReceiver
+import com.krolikowski.pomodorotimerapp.others.TimerService
 import com.krolikowski.pomodorotimerapp.ui.viewmodels.PomodoroTimerViewModel
 import com.krolikowski.pomodorotimerapp.ui.viewmodels.QuickPomodoroViewModel
 import kotlinx.android.synthetic.main.fragment_quick_pomodoro.*
-import kotlinx.coroutines.*
-import java.util.*
 
 class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
@@ -27,6 +22,8 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
     private lateinit var preferenceViewModel: QuickPomodoroViewModel
     private lateinit var pomodoroTimerViewModel: PomodoroTimerViewModel
+
+    private lateinit var serviceIntent: Intent
 
     private lateinit var timer: CountDownTimer
     private var timerLengthSeconds = 0L
@@ -39,6 +36,7 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
         pomodoroTimerViewModel = PomodoroTimerViewModel(requireContext())
         preferenceViewModel = ViewModelProvider(this).get(QuickPomodoroViewModel::class.java)
+
         getPreferences()
 
     }
@@ -48,10 +46,11 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
         pomodoro_name_TV.text = "Quick Pomodoro"
 
+        serviceIntent = Intent(requireContext(), TimerService::class.java)
+        requireContext().registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATE))
 
         start_fab.setOnClickListener {
             startPomodoro()
-
         }
 
         pause_fab.setOnClickListener {
@@ -64,34 +63,10 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
     }
 
-    override fun onPause() {
-        super.onPause()
-
-        if(timerState == "Running"){
-            timer.cancel()
-            val wakeUpTime = setAlarm(requireContext(), nowSeconds, timerSecondsRemaining)
-        } else if (timerState == "Paused"){
-
-        }
-
-        pomodoroTimerViewModel.saveTimerPreviousLength(timerLengthSeconds)
-        pomodoroTimerViewModel.saveTimerSecondsRemaining(timerSecondsRemaining)
-        pomodoroTimerViewModel.saveTimerState(timerState)
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        initTimer()
-        updateButtons()
-        removeAlarm(requireContext())
-
-    }
-
     private fun getPreferences(){
         preferenceViewModel.readFromDataStoreTime.observe(this, {
             timerLengthSeconds = it.toLong()
+            updateUI(timerLengthSeconds)
             Log.d("DEBUG",it)
         })
 
@@ -102,68 +77,16 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
         })
     }
 
-    private fun initTimer(){
-
-        lifecycleScope.launch(Dispatchers.Main) {
-            timerState = pomodoroTimerViewModel.getTimerState()
-
-            Log.d(deb_pom, timerState)
-
-            if (timerState == "Stopped"){
-                setNewTimer()
-            } else{
-                setPreviousTimer()
-            }
-
-            timerSecondsRemaining = if(timerState == "Running" || timerState == "Paused"){
-                pomodoroTimerViewModel.getTimerSecondsRemaining()
-            }else{
-                timerLengthSeconds
-            }
-
-            val alarmSetTime = pomodoroTimerViewModel.getTimerAlarmTime()
-            if (alarmSetTime > 0){
-                timerSecondsRemaining -= nowSeconds - alarmSetTime
-            }
-
-            if (timerSecondsRemaining <= 0){
-                onTimerFinished()
-            } else if (timerState == "Running"){
-                startPomodoro()
-            }
-            updateButtons()
-            updateUI()
-        }
-
-    }
-
     private fun startPomodoro(){
 
-        timerState = "Running"
+        if(timerState == "Stopped") {
+            serviceIntent.putExtra("TIME_TO_COUNT", timerLengthSeconds)
+            Log.d("DEBUG2", timerLengthSeconds.toString())
+            requireContext().startService(serviceIntent)
+            updateButtons()
+            timerState = "Running"
+        } else if (timerState == "Paused"){
 
-        timer = object : CountDownTimer(timerSecondsRemaining * 1000, 1000){
-            override fun onTick(millisUntilFinished: Long) {
-                timerSecondsRemaining = millisUntilFinished / 1000
-                updateUI()
-            }
-
-            override fun onFinish() = onTimerFinished()
-        }.start()
-        updateButtons()
-
-    }
-
-    private fun setNewTimer(){
-        lifecycleScope.launch(Dispatchers.Main) {
-            val lengthInMinutes = preferenceViewModel.readQuickPomodoroTime().toLong()
-            timerLengthSeconds = (lengthInMinutes * 60L)
-        }
-    }
-
-    private fun setPreviousTimer(){
-
-        lifecycleScope.launch(Dispatchers.Main){
-            timerLengthSeconds = pomodoroTimerViewModel.getTimerPreviousLength()
         }
 
     }
@@ -171,8 +94,7 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
     private fun pausePomodoro(){
 
         timerState = "Paused"
-
-        timer.cancel()
+        requireContext().stopService(serviceIntent)
         updateButtons()
 
     }
@@ -181,7 +103,7 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
         timerState = "Stopped"
         updateButtons()
-        timer.cancel()
+
 
 
     }
@@ -190,67 +112,45 @@ class QuickPomodoroFragment: Fragment(R.layout.fragment_quick_pomodoro) {
 
     }
 
-    private fun onTimerFinished(){
-        timerState = "Stopped"
-
-        //setNewTimer()
-
-    }
-
-    private fun updateUI(){
-        val minutesUntilFinished = timerSecondsRemaining/60
-        val secondsInMinuteUntilFinished = timerSecondsRemaining - minutesUntilFinished * 60
-        val secondsStr = secondsInMinuteUntilFinished.toString()
-        main_timer.text =
-            "$minutesUntilFinished:${
-            if (secondsStr.length == 2) 
-                secondsStr
-            else "0" + secondsStr}"
-    }
-
     private fun updateButtons(){
         when(timerState){
             "Running" ->{
                 start_fab.isEnabled = false
                 stop_fab.isEnabled = true
-                pause_fab.isEnabled = true
+                stop_fab.isEnabled = true
             }
             "Stopped" ->{
                 start_fab.isEnabled = true
                 stop_fab.isEnabled = false
-                pause_fab.isEnabled = false
+                stop_fab.isEnabled = false
             }
             "Paused" ->{
                 start_fab.isEnabled = true
                 stop_fab.isEnabled = true
-                pause_fab.isEnabled = false
+                stop_fab.isEnabled = false
             }
         }
     }
 
-    fun setAlarm(context: Context, nowSeconds: Long, secondsRemaining: Long): Long{
-        val wakeUpTime = (nowSeconds + secondsRemaining) * 1000
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(context, TimerExpiredReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, wakeUpTime, pendingIntent)
-        lifecycleScope.launch(Dispatchers.IO){
-            pomodoroTimerViewModel.saveTimerAlarmTime(nowSeconds)
-        }
-        return wakeUpTime
-    }
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val receivedTime = intent!!.getLongExtra(TimerService.TIME_EXTRA, 0L)
+            updateUI(receivedTime)
 
-    fun removeAlarm(context: Context){
-        val intent = Intent(context, TimerExpiredReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.cancel(pendingIntent)
-        lifecycleScope.launch(Dispatchers.IO){
-            pomodoroTimerViewModel.saveTimerAlarmTime(0)
+
         }
     }
 
-    val nowSeconds: Long
-        get() = Calendar.getInstance().timeInMillis / 1000
+    private fun updateUI(newTime: Long){
+        val minutesUntilFinished = newTime/60
+        val secondsInMinuteUntilFinished = newTime - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        main_timer.text =
+            "$minutesUntilFinished:${
+                if (secondsStr.length == 2)
+                    secondsStr
+                else "0" + secondsStr}"
+
+    }
 
 }
